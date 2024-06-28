c04507ed42a42a79e47535da01219aa3a7ac6633
saliq03 <javidsaliq@gmail.com>
2024-06-28 12:00:33 +0530

    homeScreen designing

--- lib/Home.dart
+++ lib/Home.dart
@@ -12,26 +12,43 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.black12,
+        color: Colors.pink,
         child: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.only(top: 30,left: 20,right: 20),
+              padding: const EdgeInsets.only(top: 60,left: 20,right: 20),
               child: Text("Flutter Courses",style: TextStyle(fontWeight: FontWeight.bold),),
             ),
             SizedBox(height: 20,),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(22)
+            Stack(
+              children: [ Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(22)
+                ),
+                width: MediaQuery.of(context).size.width,
+                height: 500,
+                child: Column(
+                  children: [
+                    Text("Saliq javid")
+                  ],
+                ),
               ),
-              width: MediaQuery.of(context).size.width,
-              height: 500,
-              child: Column(
-                children: [
-                  Text("Saliq javid")
-                ],
-              ),
+                Container(
+                  margin: EdgeInsets.only(top: MediaQuery.of(context).size.height/7),
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(22)
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  height: 663.101,
+                  child: Column(
+                    children: [
+                      Text("Saliq javid")
+                    ],
+                  ),
+                )
+              ],
             )
           ],
         )
